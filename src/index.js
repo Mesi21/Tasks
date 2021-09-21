a03e05366c78d1c890fc4b68d8a8069a98c724f5
Mesi <mse212002@gmail.com>
2021-09-21 11:05:15 +0300

    :sparkles: added displayTasks function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const tasks = [
+  {
+    description: 'task1',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'task2',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 2
+  },
+]
+
+const displayTasks = () => {
+  const listItems = document.getElementById('listItems');
+  const list = document.createElement('ul');
+  list.className = ('toDos');
+  tasks.forEach((task) => {
+    const row = document.createElement('li')
+    row.innerHTML = `${task.description}`
+    list.appendChild(row);
+  })
+  listItems.appendChild(list);
+  return listItems;
+};
+
+document.body.appendChild(displayTasks());
