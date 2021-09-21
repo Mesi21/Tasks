1bff7704c5e73fb9cb937bdbcbd371fd94dd850a
Mesi <mse212002@gmail.com>
2021-09-21 21:12:46 +0300

    :sparkles: added nav.js, :art: added basic styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import _ from 'lodash';
 import './style.css';
+import { navBar } from './nav.js';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 const tasks = [
   {
@@ -21,15 +26,48 @@ const tasks = [
 
 const displayTasks = () => {
   const listItems = document.getElementById('listItems');
+  const header = document.createElement('div');
+  const add = document.createElement('div');
+  const addNewTodo = document.createElement('input');
+  const newBtn = document.createElement('button');
+  const clearAllFinished = document.createElement('div');
+  header.setAttribute('id', 'header-ele');
+  header.innerHTML = `Today's todos`;
+  add.setAttribute('id','add-new')
+  addNewTodo.setAttribute('type', 'text');
+  addNewTodo.setAttribute('placeholder', 'Add to your list');
+  newBtn.setAttribute('type', 'submit');
+  newBtn.setAttribute('id', 'submit');
+  newBtn.innerHTML = 'Add';
+  add.appendChild(addNewTodo);
+  add.appendChild(newBtn);
+  listItems.appendChild(header);
+  listItems.appendChild(add);
   const list = document.createElement('ul');
   list.className = ('toDos');
   tasks.forEach((task) => {
     const row = document.createElement('li')
-    row.innerHTML = `${task.description}`
+    const input = document.createElement('input');
+    const dots = document.createElement('i');
+    let label = document.createElement("label");
+    dots.setAttribute('class', "fas fa-ellipsis-v");
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('value', task.description);
+    input.setAttribute('id', task.index);
+    console.log(input)
+    label.htmlFor = `${task.index}`; 
+    label.innerText = `Task ${task.index + 1}`;
+    row.appendChild(label);
+    row.appendChild(input);
+    row.appendChild(dots);
     list.appendChild(row);
   })
+  clearAllFinished.setAttribute('id', 'clear');
+  clearAllFinished.innerHTML = 'Clear all completed';
   listItems.appendChild(list);
+  listItems.appendChild(clearAllFinished);
   return listItems;
 };
 
+document.body.appendChild(navBar());
 document.body.appendChild(displayTasks());
