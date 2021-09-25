865969fdd99bd73f3fcbacdc6ea6e3b304911d82
Mesi <mse212002@gmail.com>
2021-09-25 12:44:12 +0300

    :sparkles: Added change event and helper function

--- src/index.js
+++ src/index.js
@@ -1,67 +1,87 @@
 import './style.css';
 import navBar from './nav.js';
+import createEle from './helpers/createEle.js';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
-const tasks = [
-  {
-    description: 'task1',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'task2',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'task3',
-    completed: false,
-    index: 2,
-  },
-];
+class Todo {
+  constructor() {
+    this.tasks = [
+      {
+        description: 'task1',
+        completed: false,
+        index: 0,
+      },
+      {
+        description: 'task2',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'task3',
+        completed: false,
+        index: 2,
+      },
+    ]; }
 
-const displayTasks = () => {
-  const listItems = document.getElementById('listItems');
-  const header = document.createElement('div');
-  const add = document.createElement('div');
-  const addNewTodo = document.createElement('input');
-  const newBtn = document.createElement('button');
-  const clearAllFinished = document.createElement('div');
-  header.setAttribute('id', 'header-ele');
-  header.innerHTML = 'Today\'s todos';
-  add.setAttribute('id', 'add-new');
-  addNewTodo.setAttribute('type', 'text');
-  addNewTodo.setAttribute('placeholder', 'Add to your list');
-  newBtn.setAttribute('type', 'submit');
-  newBtn.setAttribute('id', 'submit');
-  newBtn.innerHTML = 'Add';
-  add.appendChild(addNewTodo);
-  add.appendChild(newBtn);
-  listItems.appendChild(header);
-  listItems.appendChild(add);
-  const list = document.createElement('ul');
-  list.className = ('toDos');
-  tasks.forEach((task) => {
-    const row = document.createElement('li');
-    const input = document.createElement('input');
-    const dots = document.createElement('i');
-    const label = document.createElement('label');
-    dots.setAttribute('class', 'fas fa-ellipsis-v');
-    input.setAttribute('type', 'checkbox');
-    input.setAttribute('value', task.description);
-    input.setAttribute('id', task.index);
-    label.htmlFor = `${task.index}`;
-    label.innerText = `Task ${task.index + 1}`;
-    row.appendChild(label);
-    row.appendChild(input);
-    row.appendChild(dots);
-    list.appendChild(row);
-  });
-  clearAllFinished.setAttribute('id', 'clear');
-  clearAllFinished.innerHTML = 'Clear all completed';
-  listItems.appendChild(list);
-  listItems.appendChild(clearAllFinished);
-  return listItems;
-};
+  localSave() {
+    localStorage.setItem('AllTasks', JSON.stringify(this.tasks));
+  } 
+  
+  toggleState(checkBox, item) {
+    if (checkBox.checked) {
+        this.item.completed = true;
+    } else {
+        this.item.completed = false;
+    };
+    return item;
+  }
 
+  displayTasks() {
+    const listItems = document.getElementById('listItems');
+    if (localStorage.getItem('AllTasks') !== null) {
+      this.tasks = JSON.parse(localStorage.getItem('AllTasks'));
+    }
+    const header = createEle('div', null, 'header-ele', 'Today\'s todos');
+    const add = createEle('div', null, 'add-new', null);
+    const addNewTodo = createEle('input', null, 'to-add', null, null);
+    const newBtn = createEle('button', null, 'submit', 'Add');
+    const clearAllFinished = createEle('div', null, 'clear', 'Clear all completed');
+    addNewTodo.setAttribute('type', 'text');
+    addNewTodo.setAttribute('placeholder', 'Add to your list');
+    newBtn.setAttribute('type', 'submit');
+    add.appendChild(addNewTodo);
+    add.appendChild(newBtn);
+    listItems.appendChild(header);
+    listItems.appendChild(add);
+    const list = createEle('ul', 'todos', 'list-todos', null);
+    this.tasks.forEach((task) => {
+      const row = createEle('li', null, null, null);
+      const input = createEle('input', null, task.index, null);
+      const dots = createEle('i', 'fas fa-ellipsis-v', null, null);
+      const label = createEle('label', null, null, `Task ${task.index + 1}`);
+      input.setAttribute('type', 'checkbox');
+      input.setAttribute('value', task.description);
+      input.checked = task.completed;
+      input.addEventListener('change', (e) => {
+        task.completed = e.target.checked;
+        this.localSave();
+      });
+      label.htmlFor = `${task.index}`;
+      row.appendChild(label);
+      row.appendChild(input);
+      row.appendChild(dots);
+      list.appendChild(row);
+    });
+    clearAllFinished.setAttribute('id', 'clear');
+    listItems.appendChild(list);
+    listItems.appendChild(clearAllFinished);
+    return listItems;
+  }
+}
 document.body.appendChild(navBar());
-document.body.appendChild(displayTasks());
+const newTodo = new Todo();
+newTodo.localSave();
+newTodo.displayTasks();
